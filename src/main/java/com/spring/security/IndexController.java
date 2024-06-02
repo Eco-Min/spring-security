1a791f452fa10567d9992d35c07df070ca4f4e44
eco_min <guldor10.b@gmail.com>
2024-06-02 23:06:59 +0900

    exceptionHandling / ExceptionTranslationFilter

--- src/main/java/com/spring/security/IndexController.java
+++ src/main/java/com/spring/security/IndexController.java
@@ -1,8 +1,26 @@
 package com.spring.security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+
+    @GetMapping("/")
+    public Authentication index(Authentication authentication) {
+        throw new RuntimeException("error");
+//        return authentication;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/denied")
+    public String denied() {
+        return "denied";
+    }
 
 }
--- src/main/java/com/spring/security/SecurityConfig.java
+++ src/main/java/com/spring/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,8 +18,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth ->
-                        auth.anyRequest().authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .exceptionHandling(exception -> exception
+                        // ExceptionTranslationFilter, sendStartAuthentication() -> exception filter 부분을 처리한다
+                        /*.authenticationEntryPoint((request, response, authException) -> {
+                            System.out.println("auth Exception: " + authException.getMessage());
+                            response.sendRedirect("/login"); // 해당 페이지는 생성 해줘야함 -> 기본이랑 다름.
+                        })*/
+                        // ExceptionTranslationFilter, sendStartAuthentication() -> exception filter 부분을 처리한다
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            System.out.println("accessDeniedException: " + accessDeniedException.getMessage());
+                            response.sendRedirect("/denied");
+                        })
                 );
         return http.build();
 
