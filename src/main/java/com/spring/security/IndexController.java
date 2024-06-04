75f97dea042fdb966f4e2c2250d892a2fc6d79d1
eco_min <guldor10.b@gmail.com>
2024-06-04 22:49:11 +0900

    import

--- src/main/java/com/spring/security/IndexController.java
+++ src/main/java/com/spring/security/IndexController.java
@@ -1,5 +1,7 @@
 package com.spring.security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
