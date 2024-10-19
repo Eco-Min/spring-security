0a84053a1f4d5b08d0d45b8cb8b53f553dabf225
eco_min <guldor10.b@gmail.com>
2024-10-19 20:20:28 +0900

    다중 설정

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+# 고급 설정
 
+## 다중 보안 설정
+- Spring security 는 여러 SecurityFilterChain @Bean 을 등록해서 다중 보안 기능을 구성할 수 있다.
+- @Order 어노테이션을 사용해서 순서를 지정할 수 있다.
+
+## 다중 보안 설정 초기화 구성
+![image](./img/초기화구성.png)
+
+## 다중 보안 설정 요청 흐름도
+- HttpSecurity 는 인스턴스별로 보안 기능이 작동 한다.
+- 요청에 따라 RequestMatcher 를 사용해서 다른 보안 설정을 적용할 수 있다.   
+
+![image](./img/흐름도.png)
--- src/main/java/com/spring/security/IndexController.java
+++ src/main/java/com/spring/security/IndexController.java
@@ -1,6 +1,5 @@
 package com.spring.security;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
-    public Authentication index(Authentication authentication) {
-        return authentication;
+    public String index(){
+        return "index";
     }
-
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+    @GetMapping("/db")
+    public String db(){
+        return "db";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+    @GetMapping("/api")
+    public String api(){
+        return "api";
+    }
 }
